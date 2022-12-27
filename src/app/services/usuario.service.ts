7db3f07bfceebfd24f5caa97ed568b4702b21c90
alexisviazzo <j.alexisviazzo@gmail.com>
2022-12-27 17:16:54 -0300

    Agrege mas estudiantes al array de estudiantes

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -9,15 +9,15 @@ export class UsuarioService {
   alumnos: Usuario[] = [
     {usuario: '1', nombre: 'Alexis', apellido: 'Viazzo', sexo: 'masculino'},
     {usuario: '2', nombre: 'Vanessa', apellido: 'Sandes', sexo: 'femenino'},
-    // {usuario: '3', nombre: 'Jonathan', apellido: 'Barcelo', sexo: 'masculino'},
-    // {usuario: '4', nombre: 'Federico', apellido: 'Viazzo', sexo: 'masculino'},
-    // {usuario: '5', nombre: 'Martin', apellido: 'Viazzo', sexo: 'masculino'},
-    // {usuario: '6', nombre: 'Helen', apellido: 'Barcelo', sexo: 'femenino'},
-    // {usuario: '7', nombre: 'Elena', apellido: 'Albano', sexo: 'femenino'},
-    // {usuario: '8', nombre: 'Andres', apellido: 'Viazzo', sexo: 'masculino'},
-    // {usuario: '9', nombre: 'Jaqueline', apellido: 'Barcelo', sexo: 'femenino'},
-    // {usuario: '10', nombre: 'Maria', apellido: 'Dutra', sexo: 'femenino'},
-    // {usuario: '11', nombre: 'Juan', apellido: 'Gomez', sexo: 'masculino'},
+    {usuario: '3', nombre: 'Jonathan', apellido: 'Barcelo', sexo: 'masculino'},
+    {usuario: '4', nombre: 'Federico', apellido: 'Viazzo', sexo: 'masculino'},
+    {usuario: '5', nombre: 'Martin', apellido: 'Viazzo', sexo: 'masculino'},
+    {usuario: '6', nombre: 'Helen', apellido: 'Barcelo', sexo: 'femenino'},
+    {usuario: '7', nombre: 'Elena', apellido: 'Albano', sexo: 'femenino'},
+    {usuario: '8', nombre: 'Andres', apellido: 'Viazzo', sexo: 'masculino'},
+    {usuario: '9', nombre: 'Jaqueline', apellido: 'Barcelo', sexo: 'femenino'},
+    {usuario: '10', nombre: 'Maria', apellido: 'Dutra', sexo: 'femenino'},
+    {usuario: '11', nombre: 'Juan', apellido: 'Gomez', sexo: 'masculino'},
   
   ];
 
